24275efaa237f6c96ad6d8528c9961382cd7009d
yuanchongjie <1010321558@qq.com>
2025-03-11 23:06:35 +0800

    ignore

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/yuanchongjie/trae/ycj007.github.io/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"技术博客简介\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"技术博客简介\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1741702572000,\"contributors\":[{\"name\":\"yuanchongjie\",\"username\":\"yuanchongjie\",\"email\":\"1010321558@qq.com\",\"commits\":1,\"url\":\"https://github.com/yuanchongjie\"}],\"changelog\":[{\"hash\":\"add2b927c6d3d34e00261a9eacfa8acd6fa07b5f\",\"time\":1741702572000,\"email\":\"1010321558@qq.com\",\"author\":\"yuanchongjie\",\"message\":\"vuepress first\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
